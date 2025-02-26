a33d2a4a47bb85d99b8f5cc722f5e60c7cc323c1
mayankjadiwal <mayankjadiwal04@gmail.com>
2025-02-26 21:45:57 +0530

    change url

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,7 +8,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async (req,res) =>{
 
-    const frontend_url = "http://localhost:5173";
+    // const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://burger28.netlify.app/";
 
     try {
         const newOrder = new orderModel({
